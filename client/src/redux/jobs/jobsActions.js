a5c8aaa57a9f353c51ca59d91c963ed4a82a4a97
Darko <darko_trpevski@yahoo.com>
2020-11-04 13:56:04 +0100

    Adding jobsReducer

---
+++ client/src/redux/jobs/jobsActions.js
@@ -0,0 +1,48 @@
+import { GET_JOBS, GET_JOBS_SUCCESS, GET_JOBS_FAILURE, CLEAR_JOBS, SHOW_DETAILS } from './jobActionTypes';
+
+const allowCors = 'https://cors-anywhere.herokuapp.com';
+const baseUrl = 'https://jobs.github.com/positions.json';
+
+export const clearJobs = () => (dispatch) => {
+  try {
+    dispatch({
+      type: CLEAR_JOBS
+    })
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+export const fetchJobs = (description = '', location = '', full_time = '', page = 1) => async(dispatch) => {
+  const URL = `${allowCors}/${baseUrl}?description=${description}&location=${location}&full_time=${full_time ? 'on' : ''}&page=${page}`;
+  console.log('Inside jobsActions fetchJobs, URL is: ', URL);
+  dispatch({ type: GET_JOBS });
+
+  try {
+    const res = await fetch(URL, {
+      headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+        'Access-Control-Allow-Origin': '*'
+      }
+    });
+    const data = await res.json();
+
+    dispatch({ type: GET_JOBS_SUCCESS, payload: data });
+  } catch (err) {
+    console.log(err.message);
+    dispatch({ type: GET_JOBS_FAILURE });
+  }
+}
+
+export const showDetails = (id) => (dispatch) => {
+  try {
+    console.log('Inside jobsActions showDetails, id is:', id);
+    dispatch({
+      type: SHOW_DETAILS,
+      payload: id
+    });
+  } catch (err) {
+    console.error(err);
+    console.log(err.message);
+  }
+};
