c975f621169a317e48aad58124e570867304678e
Darko <darko_trpevski@yahoo.com>
2020-11-04 12:19:03 +0100

    Adding Routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 //Redux
 import { Provider } from 'react-redux';
 import store from './redux/store';
+//Components
+import Header from './components/Header/Header';
+//Import Routes
+import Routes from './Routes/Routes';
 
 const App = () => {
   return (
     <Provider store = {store}>
       <div className = "App">
-        <h1>Hello World</h1>
+        <Header/>
+        <Routes />
       </div>
     </Provider>
   );
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <header className = "Header">
+      <div className="container">
+        <h1>GitHub Job Search</h1>
+        
+      </div>
+    </header>
+  )
+}
+
+export default Header;
